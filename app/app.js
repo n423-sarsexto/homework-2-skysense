27df7a485f31df7da01f96625e9b261ae9c52ba6
Summer S <snowykittin@gmail.com>
2023-09-04 23:01:09 -0400

    Page setup

--- app/app.js
+++ app/app.js
@@ -1,3 +1,16 @@
+//navigation JS
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    $("#sidebarNav").css("width","250px");
+    $(".main").css("margin-left","250px");
+}
+  
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    $("#sidebarNav").css("width","0");
+    $(".main").css("margin-left","0");
+}
+
 //https://api.weatherapi.com/v1/current.json?key=4ccde8f188014b72810193654232808&q=46202&aqi=no
 const baseURL = `https://api.weatherapi.com/v1/forecast.json?key=`;
 const apiKey = `4ccde8f188014b72810193654232808`;
