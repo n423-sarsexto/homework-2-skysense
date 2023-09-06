0db7ebc0b7e3200ea4dc4110d2f76438baccee43
Summer S <snowykittin@gmail.com>
2023-09-06 14:58:04 -0400

    Styling and updates

--- app/app.js
+++ app/app.js
@@ -11,6 +11,8 @@ function closeNav() {
     $(".main").css("margin-left","0");
 }
 
-//https://api.weatherapi.com/v1/current.json?key=4ccde8f188014b72810193654232808&q=46202&aqi=no
+//https://api.weatherapi.com/v1/forecast.json?key=4ccde8f188014b72810193654232808&q=46239&days=5&aqi=no&alerts=no
+//set API call defaults
 const baseURL = `https://api.weatherapi.com/v1/forecast.json?key=`;
 const apiKey = `4ccde8f188014b72810193654232808`;
+const apiVals = `&days=5&aqi=no&alerts=no`;
