8c1d3da7b969bfe55715b34e05906c745868d11a
Summer S <snowykittin@gmail.com>
2023-09-08 20:24:13 -0400

    Shift of mostly js-injected code

--- app/app.js
+++ app/app.js
@@ -34,16 +34,38 @@ function getLocation(){
 
 function setWeather(weatherData){
     // current details
-    $(".current-weather-img").html(`<img src="${weatherData.current.condition.icon}" alt="${weatherData.current.condition.text}">`)
-    $(".current .location-name").html(`Currently in ${weatherData.location.name}`);
-    $(".current .last-updated").html(`Last updated ${weatherData.current.last_updated} ${weatherData.location.tz_id} time`);
-    $(".present-details .degrees-f").html(weatherData.current.temp_f);
-    $(".present-details .degrees-c").html(weatherData.current.temp_c);
-    $(".feels-like .degrees-f").html(weatherData.current.feelslike_f);
-    $(".feels-like .degrees-c").html(weatherData.current.feelslike_c);
-    $(".addtl-present .rain").html(`Precipitation: ${weatherData.current.precip_in}%`);
-    $(".addtl-present .humidity").html(`Humidity: ${weatherData.current.humidity}%`);
-    $(".addtl-present .wind").html(`Wind: ${weatherData.current.wind_mph}mph ${weatherData.current.wind_dir}`);
+    $(".current").html(`<div class="split-1">
+    <h2><span class="location-name">Currently in ${weatherData.location.name}</span></h2>
+    <div class="present-details">
+        <div class="current-weather-img"><img src="${weatherData.current.condition.icon}" alt="${weatherData.current.condition.text}"></div>
+        <h3><span class="degrees-f">${weatherData.current.temp_f}</span><sup>°F</sup> &nbsp; | &nbsp; <span
+                class="degrees-c">${weatherData.current.temp_c}</span><sup>°C</sup></h3>
+        <p class="feels-like">Feels like:</br>
+            <span class="degrees-f">${weatherData.current.feelslike_f}</span><sup>°F</sup> &nbsp; | &nbsp; <span
+                class="degrees-c">${weatherData.current.feelslike_c}</span><sup>°C</sup>
+        </p>
+    </div>
+    <div class="addtl-present">
+        <p class="rain">Precipitation: ${weatherData.current.precip_in}%</p>
+        <p class="humidity">Humidity: ${weatherData.current.humidity}%</p>
+        <p class="wind">Wind: ${weatherData.current.wind_mph}mph ${weatherData.current.wind_dir}</p>
+    </div>
+</div>
+<div class="split-2">
+    <p class="last-updated">Last updated ${weatherData.current.last_updated} ${weatherData.location.tz_id} time</p>
+    <p>Enter a location to get the weather data.</p>
+</div>`);
+
+    // future details
+    //first, reset old future forecast
+    $(".future").html("");
+    //then, get new future forecast data
+    for(let i = 1; i < weatherData.forecast.forecastday.length; i++){
+        //for each, append the elements inside of a new "day" element
+        $(".future").append(`<div class="day">
+            <div class="date">${weatherData.forecast.forecastday[i].date}</div>
+        </div>`);
+    }
 }
 
 function initListeners(){
